168a263c5eb59961561fec517d0dc156a01f16ce
Christian <papjuhles@gmail.com>
2018-04-22 16:07:26 +0200

    Update

--- Parsing_Test_complete.py
+++ Parsing_Test_complete.py
@@ -1,10 +1,14 @@
 import xml.etree.cElementTree as ET
 
-tree = ET.ElementTree(file='C:/Users/Christian/Google Drive/Uni/Master/5 Wintersemester 17-18/Masterarbeit/BPI Challenge 2017/Data/Application Event Log/BPI Challenge 2017/BPI Challenge 2017')
+tree = ET.ElementTree(file='C:/Users/Christian/Google Drive/Uni/Master/5 Wintersemester 17-18/Masterarbeit/BPI Challenge 2017/Data/Application Event Log/BPI Challenge 2017/BPI Challenge 2017.xes')
 root = tree.getroot()
 
-for entry in root:
-    if entry.get('key') == 'meta_concept:named_events_total':
-        test = entry.get('value')
+def totalEvents():
+    for entry in root:
+        if entry.get('key') == 'meta_concept:named_events_total':
+            test = entry.get('value')
+            return(test)
 
-print(test)
+
+if __name__ == '__main__':
+    print(totalEvents())
