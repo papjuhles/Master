2a1530cb5389f90385eda1b4af6da2599b31da5c
Christian <papjuhles@gmail.com>
2018-04-22 15:43:27 +0200

    Update

--- Parsing_Test_complete.py
+++ Parsing_Test_complete.py
@@ -1,6 +1,6 @@
 import xml.etree.cElementTree as ET
 
-tree = ET.ElementTree(file='C:\Users\Christian\Google Drive\Uni\Master\5 Wintersemester 17-18\Masterarbeit\BPI Challenge 2017\Data\Application Event Log\BPI Challenge 2017.xes')
+tree = ET.ElementTree(file='C:/Users/Christian/Google Drive/Uni/Master/5 Wintersemester 17-18/Masterarbeit/BPI Challenge 2017/Data/Application Event Log/BPI Challenge 2017/BPI Challenge 2017')
 root = tree.getroot()
 
 for entry in root:
